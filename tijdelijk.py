cf96317a4d07114d9d5ddf1c321090bcec11c16c
under construction <kalenganleader@yahoo.fr>
2023-05-27 15:58:53 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,78 @@
+
+#vraag  2
+mijn_prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5,
+}
+dict = {"key1" : 3, "key2" : 4, "key3" : 5}
+print("Current Dict is: ", dict)
+
+#vraag 3
+aanbieding = 4
+print (4 * 0.8)
+
+#vraag 4
+reclame_tekst = " Vandag in de aanbieding: vanille-ijs, 1 liter - slechts € aanbieding "
+print(f"Vandag in de aanbieding: vanille-ijs, 1 liter - slechts € aanbieding")
+
+#vraag 5
+reclame_tekst2 = ["zero", "one"]
+index = reclame_tekst2.index("one")
+print("The index of one:", index)
+
+#vraag 6
+reclame_tekst3 = ["ZERO", "ONE"]
+index = reclame_tekst3.index("ONE")
+print("The index of one:", index)
+
+#vraag 7
+reclame_tekst4 = ["ZERO", "ONE"]
+
+#vraag 8
+el = ["ZERO", "ONE"]
+for item in el:
+    print(item)
+
+
+#vraag 9
+mystr = ""
+print(mystr.lower())
+
+elstr = "ZERO"
+print(elstr.lower())
+
+elstr = "ONE"
+print(elstr.lower())
+
+#vraag 10
+mystr = ""
+print(mystr.lower())
+
+elstr = "ZERO"
+print(elstr.lower())
+
+elstr = "ONE"
+print(elstr.lower())
+
+for item in elstr:
+    if item in elstr:
+        print(elstr.lower())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
