b6ac9687493e3eaf63d626cae00ef66c584ba0ca
under construction <kalenganleader@yahoo.fr>
2023-06-04 12:00:00 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,63 +1,85 @@
 
 #vraag  2
-mijn_prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5,
+from helper import decoreer
+
+def print_aanbieding():
+    mijn_prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5,
+    }
+    dict = {"key1" : 3, "key2" : 4, "key3" : 5}
+    print("Current Dict is: ", dict)
+
+    #vraag 3
+    aanbieding = mijn_prijzen ["vanille"] * 0.8
+
+    #vraag 4
+    reclame_tekst = f"Vandag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    print(reclame_tekst)
+
+    #vraag 5
+    reclame_tekst2 = reclame_tekst[:63]
+    print(reclame_tekst2)
+
+    #vraag 6
+    reclame_tekst3 = reclame_tekst2.upper()
+    print(reclame_tekst3)
+
+    #vraag 7
+    reclame_tekst4 = reclame_tekst3.split()
+    print(reclame_tekst4)
+
+    #vraag 8
+    el = ["ZERO", "ONE"]
+    for item in el:
+        print(item)
+
+
+    #vraag 9
+    mystr = ""
+    print(mystr.lower())
+
+    elstr = "ZERO"
+    print(elstr.lower())
+
+    elstr = "ONE"
+    print(elstr.lower())
+
+    #vraag 10
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+            decoreer("Aanbieding")
+            print_aanbieding()
+
+prijzen = {
+"aardbei" : 3,
+"vanille" : 4,
+"chocolade" : 5
 }
-dict = {"key1" : 3, "key2" : 4, "key3" : 5}
-print("Current Dict is: ", dict)
+aanbieding = prijzen["vanille"] * 0.8
 
-#vraag 3
-aanbieding = 4
-print (4 * 0.8)
+reclame_tekst = f"Vandaag in de aanbieding: Vanille-ijs, 1 Liter – slechts € {aanbieding}"
+print(reclame_tekst)
 
-#vraag 4
-reclame_tekst = " Vandag in de aanbieding: vanille-ijs, 1 liter - slechts € aanbieding "
-print(f"Vandag in de aanbieding: vanille-ijs, 1 liter - slechts € aanbieding")
+reclame_tekst2 = reclame_tekst[:63]
+print(reclame_tekst2)
 
-#vraag 5
-reclame_tekst2 = ["zero", "one"]
-index = reclame_tekst2.index("one")
-print("The index of one:", index)
+reclame_tekst3 = reclame_tekst2.upper()
+print(reclame_tekst3)
 
-#vraag 6
-reclame_tekst3 = ["ZERO", "ONE"]
-index = reclame_tekst3.index("ONE")
-print("The index of one:", index)
+reclame_tekst4 = reclame_tekst3.split()
+print(reclame_tekst4)
 
-#vraag 7
-reclame_tekst4 = ["ZERO", "ONE"]
-
-#vraag 8
-el = ["ZERO", "ONE"]
-for item in el:
-    print(item)
-
-
-#vraag 9
-mystr = ""
-print(mystr.lower())
-
-elstr = "ZERO"
-print(elstr.lower())
-
-elstr = "ONE"
-print(elstr.lower())
-
-#vraag 10
-mystr = ""
-print(mystr.lower())
-
-elstr = "ZERO"
-print(elstr.lower())
-
-elstr = "ONE"
-print(elstr.lower())
-
-for item in elstr:
-    if item in elstr:
-        print(elstr.lower())
+for el in reclame_tekst4:
+  if len(el) > 4:
+    print(el.upper())
+else:
+    print(el.lower())
 
 
 
